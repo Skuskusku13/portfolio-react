7251a90d07f20c0b47751782ddcb03cbcc8969ff
Skuskusku13 <danlevy149@gmail.com>
2025-02-06 22:07:17 +0100

    maj name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Portfolio en cours de développement
         </p>
         <a
           className="App-link"
